3c60109e268ad997c8a9f44d7dd0a72e48719b58
smaeyama <maeyama.shinya@nifs.ac.jp>
2026-03-17 16:01:47 +0900

    Add JAX lingk solver with DLRA checks and plotting tools

--- cleaner.sh
+++ cleaner.sh
@@ -5,6 +5,7 @@ find . -name __pycache__ | xargs rm -rf
 #rm -rf build/
 #rm -rf src/bzx.egg-info/
 rm -rf examples/output/
+rm -rf *.nc
 #rm -f tests/log.dat tests/temp.nc
 rm -rf .pytest_cache/
 rm -rf .venv/
